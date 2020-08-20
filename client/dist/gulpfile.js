baeb336b9e9aae531377f70b1fa5d89baeec1ac6
Jake Turchetta <14jturchetta@gmail.com>
2020-08-20 14:58:18 -0400

    APIs configured, React components integrated, data ready

--- client/dist/gulpfile.js
+++ client/dist/gulpfile.js
@@ -1,1 +1,27 @@
 /* Gulp should compile sass and make client side JS compatible - feel free to add your own tasks! */
+'use strict'
+
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const del = require('del')
+
+gulp.task('styles', () => {
+    return gulp.src('sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'))
+})
+
+gulp.task('clean', () => {
+    return del([
+        'css/main.css',
+    ])
+})
+
+gulp.task('default', gulp.series(['clean', 'styles']))
+
+gulp.task('watch', () => {
+  gulp.watch('sass/*.scss', (done) => {
+      gulp.series(['clean', 'styles'])(done)
+  })
+})
+
