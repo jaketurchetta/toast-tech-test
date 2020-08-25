3d08d6984e57efc726dc5bb55f2046642ad48c84
Jake Turchetta <14jturchetta@gmail.com>
2020-08-25 17:10:02 -0400

    height and width adjustment

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3fd81e339cdfea599519";
+/******/ 	var hotCurrentHash = "e3f138e694be06e8a4a8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
