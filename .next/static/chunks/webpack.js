085d9605fffca6eb944cc89a4c238db8f262d74b
Jake Turchetta <14jturchetta@gmail.com>
2020-08-25 12:41:11 -0400

    relative title font size

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6dc64a5c77b79a06e50b";
+/******/ 	var hotCurrentHash = "8b3a46d7b1e8bfe2bb70";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
