78893352bbcb0f052ea0df11d3804b2525c2345d
Jake Turchetta <14jturchetta@gmail.com>
2020-08-25 16:13:33 -0400

    resize weather icons

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "502bdaa348d3b58722b9";
+/******/ 	var hotCurrentHash = "e732e3dd62326c4812d7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
