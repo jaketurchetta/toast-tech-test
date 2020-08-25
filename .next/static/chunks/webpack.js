2422b8be714e9b5b2c469d1b02678107fd679072
Jake Turchetta <14jturchetta@gmail.com>
2020-08-25 17:05:51 -0400

    finishing touches

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ff5640e2a49b93233ef8";
+/******/ 	var hotCurrentHash = "3fd81e339cdfea599519";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
